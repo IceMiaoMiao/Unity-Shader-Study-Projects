82d1b966ac63f9ec999a2aec6ae4045a2c7ab63a
yangyang guo <94036888+IceMiaoMiao@users.noreply.github.com>
2022-07-04 18:34:50 +0800

    边缘检测

---
+++ Assets/Scripts/EdgeDetection.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetection : PostEffectsBase
+{
+    public Shader edgeDetectShader;
+    private Material edgeDetectMaterial=null;
+
+    public Material material
+    {
+        get
+        {
+            edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+            return edgeDetectMaterial;
+        }
+    }
+
+    [Range(0, 1)] public float edgesOnly = 0.0f;
+    public Color edgeColor = Color.black;
+    public Color backgroundColor=Color.white;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material!=null)
+        {
+            material.SetFloat("_EdgeOnly",edgesOnly);
+            material.SetColor("_EdgeColor",edgeColor);
+            material.SetColor("_BackgroundColor",backgroundColor);
+            Graphics.Blit(src,dest,material);
+            
+        }
+        else
+        {
+            Graphics.Blit(src,dest);
+        }
+    }
+}
